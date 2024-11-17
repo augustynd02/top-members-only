9031c1a4189fea5059620efdd205449288442b41
yurishe0 <yurishe00@gmail.com>
2024-11-17 15:12:13 +0100

    fix: properly mount the router with app.use instead of app.get

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const indexRouter = require('./routes/indexRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', indexRouter);
-
+app.use('/', indexRouter);
 app.listen(3000, () => console.log('Server running on port 3000...'));
