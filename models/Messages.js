9d9209392ad91aa4a98cbb3d72cee020923cccf3
yurishe0 <yurishe00@gmail.com>
2024-11-23 00:12:17 +0100

    fix: correctly deconstruct the returned data from the query

--- models/Messages.js
+++ models/Messages.js
@@ -2,8 +2,8 @@ const pool = require('../config/database');
 
 const Messages = {
     getAllMessages: async (req, res) => {
-        const result = pool.query("SELECT * FROM messages;");
-        return result;
+        const { rows } = await pool.query("SELECT * FROM messages;");
+        return rows;
     }
 }
 
