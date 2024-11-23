00908a60e2bb7f3ae2e1a7c64ecbc1cd67007478
yurishe0 <yurishe00@gmail.com>
2024-11-23 13:58:57 +0100

    feat: use the validator inside the register route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,11 +3,23 @@ const indexRouter = Router();
 const indexController = require('../controllers/indexController');
 const passport = require('passport');
 const { isAuth } = require('../middleware/isAuth');
+const { validateRegister } = require('../middleware/validation');
+const { validationResult } = require('express-validator');
 require('../config/passport');
 
 indexRouter.get('/', indexController.getIndex);
 indexRouter.get('/register', indexController.getRegister);
-indexRouter.post('/register', indexController.postRegister);
+indexRouter.post('/register', validateRegister, (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed.')
+        error.status = 400;
+        error.details = errors.array();
+        next(error)
+    } else {
+        next();
+    }
+}, indexController.postRegister);
 indexRouter.get('/login', indexController.getLogin);
 indexRouter.post('/login', passport.authenticate("local", {
         successRedirect: "/",
