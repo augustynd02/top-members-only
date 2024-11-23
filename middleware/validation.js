a936308701ed2bc1ad9608916eaaaf1a86f75185
yurishe0 <yurishe00@gmail.com>
2024-11-23 13:58:28 +0100

    feat: use express-validator dependency to validate forms

---
+++ middleware/validation.js
@@ -0,0 +1,33 @@
+const { body } = require("express-validator");
+
+const validateRegister = [
+    body('username')
+        .trim()
+        .notEmpty().withMessage('Username is required.')
+        .isLength({ min: 2, max: 255 }).withMessage('Username must be at least 2 characters long.'),
+    body('firstname').trim()
+        .notEmpty().withMessage('First name is required.')
+        .isLength({ min: 2, max: 255 }).withMessage('First name must be at least 2 characters long.'),
+
+    body('lastname')
+        .trim()
+        .optional()
+        .isLength({ min: 2, max: 255 }).withMessage('Last name must be at least 2 characters long.'),
+
+    body('password')
+        .trim()
+        .notEmpty().withMessage('Password is required.')
+        .isLength({ min: 4, max: 255 }).withMessage('Password must be at least 4 characters long.'),
+
+    body('confirmpassword').trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords must be identical');
+            }
+            return true;
+        }),
+]
+
+module.exports = {
+    validateRegister
+}
