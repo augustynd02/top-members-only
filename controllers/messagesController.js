25bf4687cf30e6e4dbc5b56a3d1f5e80413ca07a
yurishe0 <yurishe00@gmail.com>
2024-11-24 12:28:04 +0100

    feat: format the date on messages to a clearer format

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,9 +1,11 @@
 const db = require('../models/Messages');
+const { formatDate } = require('../utils/formatDate');
 
 const messagesController = {
     getMessages: async (req, res) => {
         const messages = await db.getAllMessages();
         console.log(messages);
+        messages.forEach(message => message.created_at = formatDate(message.created_at));
         res.render('pages/messages', { messages: messages });
     },
     getNewMessage: (req, res) => {
