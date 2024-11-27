61d7ca96f6ce58104b95b453db7c3156a0e48e54
yurishe0 <yurishe00@gmail.com>
2024-11-27 18:24:54 +0100

    misc: remove debugging line

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,6 @@ const db = require('../models/User');
 const usersController = {
     getUsers: async (req, res) => {
         const users = await db.getAllUsers();
-        console.log(users);
         res.render('pages/users', { users: users });
     },
     getDeleteUser: async (req, res) => {
